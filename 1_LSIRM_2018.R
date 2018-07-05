971fb29d3fb841a11c6845e726ebd16d44a7910f
Simon Kiss <sjkiss@gmail.com>
2018-07-05 09:53:59 -0400

    Commit morning July 5

---
+++ 1_LSIRM_2018.R
@@ -0,0 +1,174 @@
+## ----setup, echo=F, results='hide'---------------------------------------
+knitr::opts_chunk$set(echo = TRUE, message=F, warning=F, fig.align='center')
+
+## ----print-example-------------------------------------------------------
+print('Hello world')
+
+## ----save-objects, results='hide'----------------------------------------
+abc<-print('Hello world')
+
+
+## ----print-saved-objects-------------------------------------------------
+abc
+
+## ----install-tidyverse, eval=F-------------------------------------------
+## install.packages('tidyverse')
+## install.packages('haven')
+## install.packages('pastecs')
+## install.packages('car')
+## install.packages('psych')
+## install.packages('stargazer')
+## install.packages('xtable')
+## install.packages('ggeffects')
+
+## ----load-tidyverse------------------------------------------------------
+library(tidyverse)
+
+## ----load-haven----------------------------------------------------------
+library(haven)
+
+## ----numeric-------------------------------------------------------------
+num<-1
+
+
+## ----character-----------------------------------------------------------
+char<-'1'
+
+## ----num-char------------------------------------------------------------
+num
+char
+
+## ----check-class---------------------------------------------------------
+class(num)
+class(char)
+
+## ----make-vectors--------------------------------------------------------
+gender<-c(0,1,0)
+age<-c(18,22,33)
+lucky<-c(1,2,3)
+
+## ----assignment-1-solution-hidden, results='hide', echo=F----------------
+matrix1<-matrix(c(age, gender, lucky), nrow=3, ncol=3)
+
+
+## ----make-data-frame, results='hide'-------------------------------------
+df<-data.frame(age=age, gender=gender, lucky=lucky)
+
+## ----access-individual-variables-----------------------------------------
+df$age
+df$gender
+
+## ----summary-commands----------------------------------------------------
+#summarize the first few rows of a data frame
+head(df)
+#Examine the structure of a data frame
+str(df)
+#Summary statistics of a data frame.
+summary(df)
+#Access one variable of a data frame
+df$age
+
+## ----individual-variables------------------------------------------------
+#Check class of each variable
+class(df$age)
+class(df$gender)
+class(df$lucky)
+
+## ----load-car------------------------------------------------------------
+library(car)
+
+## ----Recode-gender-------------------------------------------------------
+Recode(df$gender, "0='male' ; 1='female'", as.factor=T)
+
+## ----Recode-gender-2-----------------------------------------------------
+df$gender2<-Recode(df$gender, 
+                   "0='male' ; 1='female'", 
+                   as.factor=T)
+
+## ----check-levels-gender-2-----------------------------------------------
+levels(df$gender2)
+
+
+## ----make-lists----------------------------------------------------------
+list1<-list(age, gender,lucky)
+
+
+## ----change-classes------------------------------------------------------
+#print the numeric variable num as a character
+as.character(num)
+#print the character variable char as a number
+as.numeric(char)
+#Convert both to a factor (categorical variable)
+as.factor(num)
+as.factor(char)
+
+## ----maths---------------------------------------------------------------
+#Addition
+x<-1+2
+x
+y<-2+1
+#Division
+x/y
+#Multiplication
+x*y
+#Exponentiation
+x^2
+#Square Root
+sqrt(9)
+
+## ----calculate-mean------------------------------------------------------
+#Calculate mean
+mean(age)
+#Calculate median
+median(age)
+#get the maximum values
+max(age)
+
+## ----function-examples, eval=F-------------------------------------------
+## #Note: the capitalization patterns for functions.
+## #Words are not separated with periods or underscores, but with capitalized names.
+## #The arguments that each function takes are specified in parentheses.
+## functionName<-function(x,y,z){ #The actual commands are opened with a curly brace command
+##   #The function commands are entered here
+## }# The function is closed here
+## 
+
+## ----assignment-1-solution, echo=TRUE------------------------------------
+matrix(c(age, gender, lucky), nrow=3, ncol=3)
+
+
+## ----assignment-2-solution, eval=F---------------------------------------
+## #The number in the square brackets indicates which element of the vector gender to return
+## age[1]
+## gender[1]
+
+## ----assignment-3-solution, eval=F---------------------------------------
+## matrix1[1,]
+## matrix1[2,]
+## matrix1[,1]
+
+## ----assignment-4-solution, eval=F---------------------------------------
+## #First through third
+## matrix1[1:3,]
+## #first and third
+## matrix1[c(1,3),]
+## 
+
+## ----assignment-5-solution, eval=F---------------------------------------
+## df<-data.frame(age=age, gender=gender, lucky=lucky)
+
+## ----assignment-6-solution, eval=F---------------------------------------
+## #Defie the function
+## myfunction<-function(i) {
+##   print(i)
+## }
+## #Print the number 7
+## myfunction(7)
+## 
+## #Print 7 but as a character.
+## test(as.character(7))
+
+## ---- results='hide', echo=F---------------------------------------------
+library(knitr)
+purl('1_LSIRM_2018.rmd', 'out.r')
+
---
+++ 2_LSIRM_2018.r
@@ -0,0 +1,273 @@
+## ----setup---------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, warning=F, message=F,  results='hide', fig.width=4, fig.height=3, fig.align='center')
+
+## ----read-in-------------------------------------------------------------
+#ces<-rio::import('https://github.com/sjkiss/LSIRM_2018/blob/master/data/ces_data.sav?raw=true')
+library(haven)
+library(labelled)
+ces<-read_sav('data/ces15_data.sav')
+
+
+
+## ---- examine-data-------------------------------------------------------
+#Different ways of summarizing the data set
+#Check the first few rows
+head(ces)
+#The last few rows
+tail(ces)
+#Check the structure of the object
+str(ces)
+#Summarize the data set
+summary(ces)
+#See the variables names
+names(ces)
+
+
+
+## ----get-labels----------------------------------------------------------
+var_label(ces)
+val_labels(ces)
+
+
+## ----assignment-2-solution-----------------------------------------------
+vars<-var_label(ces)
+vals<-val_labels(ces)
+
+## ----assignment-3-solution-----------------------------------------------
+vals
+
+## ---- eval=F, echo=T-----------------------------------------------------
+## Recode(x, "old value=new value")
+
+## ----results='markup', echo=T--------------------------------------------
+#load the car library
+library(car)
+#Recode 1
+test<-car::Recode(as.numeric(ces$p_incineq),"3:1000=NA", as.numeric=T)
+#Recode 2
+test2<-Recode(as.numeric(ces$p_incineq), "3=NA; 4=NA; 998=NA; 1000=NA", as.numeric=T)
+#Summary test
+summary(test)
+#summary test 2
+summary(test2)
+
+
+## ---- results='hide', echo=T---------------------------------------------
+ces$inequal<-Recode(as.numeric(ces$p_incineq), "3:1000=NA")
+str(ces)
+
+## ----assignment-4-solution-----------------------------------------------
+#detach('package:dplyr')
+
+
+ces$education<-Recode(as.numeric(ces$education), "12:1000=NA")
+
+vals
+ces$p_votechce<-Recode(as.numeric(ces$p_votechce), "8:1000=NA")
+vals
+ces$pers_ret<-Recode(as.numeric(ces$pers_ret), "4:1000=NA")
+vals
+ces$pers_fdpol<-Recode(as.numeric(ces$pers_fdpol), "4:1000=NA")
+ces$p_gap<-Recode(as.numeric(ces$p_gap), "6:1000=NA")
+ces$sex_r<-Recode(as.numeric(ces$sex_r), "3:1000=NA")
+ces$income<-Recode(as.numeric(ces$income_full), "6:1000=NA")
+str(ces)
+
+## ----summary-ces---------------------------------------------------------
+summary(ces)
+
+## ----assignment-solution-5-----------------------------------------------
+#Recode p_selfplace
+ces$ideology<-Recode(as.numeric(ces$p_selfplace), "1000=NA")
+str(ces)
+
+## ----results='markup'----------------------------------------------------
+library(psych)
+desc<-describe(ces)
+
+## ----load-tidyverse------------------------------------------------------
+library(tidyverse)
+
+library(ggplot2)
+library(dplyr)
+
+## ---- make-plot, fig.cap='No plot!'--------------------------------------
+
+
+ggplot(ces, aes(x=income))
+
+
+## ----income-bar,fig.cap='Histogram', fig.align='center'------------------
+#Add barplot
+
+  ggplot(ces, aes(x=income))+
+  geom_bar()
+
+
+## ----assignment-6-solution-----------------------------------------------
+
+plot1<-  ggplot(ces, aes(x=ideology))+geom_density()
+
+
+## ----show-aesthetics-----------------------------------------------------
+plot1+geom_density(col='red')
+plot1+geom_density(size=3)
+
+## ----linetype-aes, fig.cap='Changing linetype on a densit plot does not do anything.'----
+plot1+geom_density(linetype=3)
+
+## ---- make-bivariate-plot, fig.cap='A basic scatterplot'-----------------
+
+
+  ggplot(ces, aes(x=income, y=ideology))+
+  geom_point()
+
+## ----assignment-solution-7-----------------------------------------------
+#Scatterplot
+plot2<-ggplot(ces, aes(x=education, y=ideology))+geom_point()
+plot2
+#Boxplot
+plot3<-ggplot(ces, aes(x=as.factor(education) ,y=ideology))+geom_boxplot()
+plot3
+
+## ----check-variables-----------------------------------------------------
+#Check sex
+ces$sex_r
+#Check the value labels of sex
+vals$sex_r
+
+## ----Recode-gender-factor------------------------------------------------
+#recode sex into gender
+ces$gender<-car::Recode(ces$sex_r, "1='M' ; 2='F'", levels=c('M', 'F'), as.factor=T)
+
+## ---- plot-4,include=T---------------------------------------------------
+#Make plot 4, save as plot4
+plot4<-ggplot(ces, aes(x=income, y=ideology, group=gender))+geom_point(aes(col=gender))
+#Print plot4
+plot4
+
+
+## ----assignment-8-solution-----------------------------------------------
+plot4a<-ggplot(ces, aes(x=income, y=ideology, group=gender))+geom_point(aes(shape=gender))
+plot4a
+
+## ----plot4b--------------------------------------------------------------
+plot4b<-plot4+geom_jitter(aes(col=gender))
+
+plot4b
+
+## ---- plot4b-smooth------------------------------------------------------
+plot4b<-plot4+geom_smooth(method='lm', aes(col=gender))
+plot4b
+
+## ----assignment-9-solution-----------------------------------------------
+#Check value labels
+vals
+#Recode eduation to degree
+ces$degree<-car::Recode(ces$education, "1:7='No degree' ; 8:11='Degree'", levels=c('No degree', 'Degree'))
+plot5<-ggplot(ces, aes(x=income, y=ideology))+geom_point(aes(col=degree))+geom_smooth(aes(col=degree), method='lm')
+plot5
+
+## ------------------------------------------------------------------------
+plot5+labs(x='Income', y='Ideology', title='Ideology on Income, CES 2015')
+
+## ----correlation---------------------------------------------------------
+names(ces)
+str(ces)
+cor(ces$ideology, ces$income, use='complete.obs')
+
+
+## ---- table-1, results='markup'------------------------------------------
+table(ces$gender, ces$inequal)
+
+
+## ----assignment-10-solution----------------------------------------------
+
+ces$inequal
+#Check values
+vals
+ces$inequal_cat<-Recode(ces$inequal, "1='Yes' ; 2='No'", as.factor=T)
+#Crosstab
+tab2<-table(ces$gender, ces$inequal_cat)
+
+## ----chi-sq-prop-table, results='markup'---------------------------------
+
+#Chi-sq.test
+chisq.test(tab2)
+#For prop.table you can produce either row (1) or column (2) percentgates
+#Compare
+prop.table(tab2, 1)
+#With
+prop.table(tab2, 2)
+
+## ------------------------------------------------------------------------
+library(cowplot)
+plot_grid(plot1, plot2, plot3, plot4)
+
+## ----lm-model1, results='markup'-----------------------------------------
+#Fit linear model
+mod1<-lm(ideology ~ income, data=ces)
+
+## ----mod2, results='markup'----------------------------------------------
+#Model 2
+mod2<-lm(ideology~income+education, data=ces)
+#summarize
+summary(mod2)
+
+## ----mod3, results='markup'----------------------------------------------
+#Model 3
+mod3<-lm(ideology~income+education+income:education, data=ces)
+#Summarize
+summary(mod3)
+
+## ---- update-mod2--------------------------------------------------------
+#New model
+mod3a<-update(mod2, .~.+income:education, data=ces)
+#Compare
+summary(mod3)
+summary(mod3a)
+
+## ----assignment-11-solution----------------------------------------------
+m1<-lm(p_gap~income, data=ces)
+m2<-update(m1, .~+ideology, data=ces)
+m3<-update(m2, .~+education, data=ces)
+m4<-update(m3, .~+gender, data=ces)
+m5<-update(m1, .~.+ideology+education+gender, data=ces)
+m6<-update(m5, .~.+gender:income+gender:ideology+gender:education, data=ces)
+summary(m1)
+summary(m2)
+summary(m3)
+summary(m4)
+summary(m5)
+summary(m6)
+
+## ----report-regressions, results='asis'----------------------------------
+#load library
+library(stargazer)
+stargazer(m1, m2, m3, m4, m5, m6, type='text')
+
+## ----export-regressions, results='hide'----------------------------------
+stargazer(m1, m2, m3, m4, m5, m6, type='html', out='model_results.html')
+
+## ----assignment-12-solution, results='asis'------------------------------
+#update m5
+m7<-update(m5, .~.+ideology:gender, data=ces)
+summary(m7)
+
+## ------------------------------------------------------------------------
+
+library(ggeffects)
+
+## ----predicted-values, results='markup'----------------------------------
+m7.preds<-ggpredict(m7, terms=c('ideology [0,3, 5,7,10]', 'gender'))
+#summarize
+m7.preds
+ggplot(m7.preds)
+ggplot(m7.preds, aes(x=x, y=predicted, col=group))+geom_point()+geom_line()+labs(x='Ideology')
+
+## ----assignment-13-solution----------------------------------------------
+m7.preds2<-ggpredict(m7, terms=c('ideology [0,3, 5,7,10]', 'income', 'gender'))
+#plot
+ggplot(m7.preds2, aes(x=x, y=predicted, col=group))+facet_grid(~facet)+geom_point()+geom_line()+labs(x='Ideology', col='Income')
+
+
