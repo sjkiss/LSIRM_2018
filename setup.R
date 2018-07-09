9b203e0cffd82c04e6ff306499699156a6d8b299
Simon Kiss <sjkiss@gmail.com>
2018-07-09 15:53:05 -0400

    Commit on July 8th.

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(rio)
-ces15<-import('OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/CES15_CPS+PES_Web_SSI Full.dta')
+ces15<-import('~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/CES15_CPS+PES_Web_SSI Full.dta')
 library(questionr)
 lookfor(ces15, 'income')
 lookfor(ces15, 'left')
@@ -10,6 +10,9 @@ lookfor(ces15, 'year')
 lookfor(ces15, 'financial')
 lookfor(ces15, 'rich')
 lookfor(ces15, 'sex')
+
 out<-ces15 %>% 
-  select(p_incineq, income_num, p_selfplace, education, p_votechce, pers_ret, pers_fdpol, p_gap, sex_r) 
-export('OneDrive - Wilfrid Laurier University/projects_folder/ces15_data.sav')
+  select(p_incineq, income_full, p_selfplace, education, p_votechce, pers_ret, pers_fdpol, p_gap, sex_r) 
+
+export(out, file='data/ces15_data.sav', format='sav')
+
