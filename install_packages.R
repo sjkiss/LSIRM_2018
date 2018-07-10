717c05b22035ac83648b3c6dd5fbdef52e2d0d98
Simon Kiss <sjkiss@gmail.com>
2018-07-10 12:37:40 -0400

    Update install.packages()

--- install_packages.R
+++ install_packages.R
@@ -1,16 +1,18 @@
 #Necessary libraries
 
-to.install<-c('pastecs', 'haven', 'labelled', 'car', 'psych', 'ggplot2', 'dplyr', 'tidyr', 'Hmisc', 'devtools', 'xtable', 'effects', 'stargazer')
+to.install<-c('car', 'haven', 'labelled', 'foreign',  'psych', 'tidyverse', 'ggeffects', 'xtable', 'stargazer', 'questionr', 'rio', 'devtools', 'effects', 'scales', 'cowplot')
 
 #Packages that have not been installed
 new.packages <- to.install[!(to.install %in% installed.packages()[,"Package"])]
 #If there are packages that must be installed, this installs them
 if(length(new.packages)) install.packages(new.packages, repos='http://cran.utstat.utoronto.ca/')
-#This loads packages
-loaded.packages<-lapply(to.install, require, character.only=T)
-loaded.packages
-#Note: after running this command, TRUE should be printed out 13 times on the console. If not, please contact Simon Kiss at skiss@wlu.ca
-#One more check
+
+#Save installed package names
+out<-to.install[to.install %in% installed.packages()[,'Package']]
+#print out
+out
+
+#You should see 'Hello World' after this. 
 print('Hello World')
 #One last check
 plot(seq(1,10,1), seq(1,10,1), main='If you can read this, you are set up.')
